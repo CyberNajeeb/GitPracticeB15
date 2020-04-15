285fa586b716083e8a641ed9fbf3b265b44b646f
Mohammed Sohrabi <najeebsohrabi@gmail.com>
2020-04-15 14:58:04 -0700

    New branch added

--- src/test/java/officeHour/GitPractice/GitPractice.java
+++ src/test/java/officeHour/GitPractice/GitPractice.java
@@ -2,4 +2,9 @@ package officeHour.GitPractice;
 
 public class GitPractice {
 
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
 }
